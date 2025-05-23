449c2f41547331caa24d6fe373122699b1992bec
Raj Krish <145284234+rajkrish0608@users.noreply.github.com>
2025-05-23 17:17:44 +0530

    Create Main.java

---
+++ 160-Day-Challenge/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Name: Raj Krish
+ * Github: rajkrish0608
+ * Linkedln: https://www.linkedin.com/in/raj-krish-3a7a3b285/
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Let's Start 160 days challenge of DSA");
+
+    }
+}
